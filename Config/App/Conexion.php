d4fdca81fd60ed7397d930d74367bea3522e0927
Emilio <jemilio_s@hotmail.com>
2022-02-02 12:33:22 -0500

    Actualizado2

--- Config/App/Conexion.php
+++ Config/App/Conexion.php
@@ -3,8 +3,15 @@
         private $conect;
         public function __construct()
         {
+            $jawsdb_url = parse_url(getenv("JAWSDB_URL"));
+            $jawsdb_server = $jawsdb_url["host"];
+            $jawsdb_username = $jawsdb_url["user"];
+            $jawsdb_password = $jawsdb_url["pass"];
+            $jawsdb_db = substr($jawsdb_url["path"],1);
+            $active_group = 'default';
+            $query_builder = TRUE;
             try{
-                $this->conect = new mysqli(host, user, pass, db);
+                $this->conect = new mysqli($jawsdb_server, $jawsdb_username, $jawsdb_password, $jawsdb_db);
             }catch(mysqli_sql_exception $e){
                 echo "Error en la conexión ".$e->getMessage();
             }
--- Config/Config.php
+++ Config/Config.php
@@ -6,13 +6,10 @@
     const pass = "sd23";
     const db = "minicore";
     const charset = "charset=utf8mb4"; */
-    $cleardb_server = $cleardb_url["host"];
-    $cleardb_username = $cleardb_url["user"];
-    $cleardb_password = $cleardb_url["pass"];
-    $cleardb_db = substr($cleardb_url["path"],1);
+    $jawsdb_server = $jawsdb_url["host"];
+    $jawsdb_username = $jawsdb_url["user"];
+    $jawsdb_password = $jawsdb_url["pass"];
+    $jawsdb_db = substr($jawsdb_url["path"],1);
     $active_group = 'default';
     $query_builder = TRUE;
-    // Connect to DB
-    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-    ?>
 ?>
