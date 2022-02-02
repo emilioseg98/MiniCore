8ce51d74a3aad0efb104b643a71de6bd855cce7c
Emilio <jemilio_s@hotmail.com>
2022-02-02 13:12:43 -0500

    Actualizado3

--- Config/App/Conexion.php
+++ Config/App/Conexion.php
@@ -4,10 +4,10 @@
         public function __construct()
         {
             $jawsdb_url = parse_url(getenv("JAWSDB_URL"));
-            $jawsdb_server = $jawsdb_url["host"];
-            $jawsdb_username = $jawsdb_url["user"];
-            $jawsdb_password = $jawsdb_url["pass"];
-            $jawsdb_db = substr($jawsdb_url["path"],1);
+            $jawsdb_server = "uzb4o9e2oe257glt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+            $jawsdb_username = "a1do883xd3tsbkvg";
+            $jawsdb_password = "xtjw27xiu6vlq6fz";
+            $jawsdb_db = "bnbmjxzfv75clk5v";
             $active_group = 'default';
             $query_builder = TRUE;
             try{
